bbca1cd2cd5f39700d0bd6d9f5514fdbf4ba9956
aravind <aravi@LAPTOP-5LCMP4L4>
2023-07-20 05:51:44 +0530

    DSA

---
+++ DSA/src/SqrtUsingBS.java
@@ -0,0 +1,28 @@
+public class SqrtUsingBS {
+
+    public static void main(String[] args) {
+        System.out.print(squareRoot(2147395599));
+    }
+
+    public static int squareRoot(int number) {
+        if (number == 0 || number == 1) {
+            return number;
+        }
+        int low = 1;
+        int high = number / 2;
+        int res = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mid * mid == number) {
+                return mid;
+            } else if (mid * mid < number) {
+                low = mid + 1;
+                res = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return res;
+    }
+}

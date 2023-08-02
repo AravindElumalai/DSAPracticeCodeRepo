af11adc87b9def2e5e111985253360767abd8397
Aravind <AravindElumalai@users.noreply.github.com>
2023-08-02 13:56:29 +0530

    Update LC35SearchInsertPosition.java

--- DSA/src/LC35SearchInsertPosition.java
+++ DSA/src/LC35SearchInsertPosition.java
@@ -1,9 +1,26 @@
+/*https://leetcode.com/problems/search-insert-position/ */
 public class LC35SearchInsertPosition {
     public static void main(String[] args) {
-
+        int[] nums = { 1, 3, 5, 6 };
+        int target = 7;
+        int res = searchInsert(nums, target);
+        System.out.println(res);
     }
 
     public static int searchInsert(int[] nums, int target) {
-        return 0;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
     }
 }
